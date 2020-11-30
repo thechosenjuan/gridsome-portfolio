04de93a63dbf2c5e5d02adf608c547cb84f8018b
Juan Flores <juan.flores@base22.com>
2020-11-30 13:24:44 -0500

    Some text adjustements

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,9 +4,14 @@
       <g-image src="~/assets/images/profile_pic.jpg" class="rounded-full" />
     </div>
     <div class="w-2/3 sm:ml-4 mt-4 sm:mt-0">
-      <p>
-        Hi there, my name is Juan and I'm a Front-End Developer and learning
-        enthusiast.
+      <p class="mb-4 text-justify">
+        Hi there, my name is Juan and I'm a Software Engineer currently focused
+        on Front End Web Development. I'm very curious person and I strive to
+        learn something new everyday.
+      </p>
+      <p class="text-justify">
+        Welcome to my little corner of the Internet. Grab a blanket and some hot
+        cocoa.
       </p>
     </div>
   </div>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,24 +1,66 @@
 <template>
   <section class="text-gray-700 mt-8">
-    <h2 class="text-center text-5xl font-bold">My Projects</h2>
+    <h2 class="text-center text-5xl font-bold mb-4">My Projects</h2>
     <div class="flex flex-wrap -m-4">
       <div class="p-4 md:w-1/2 lg:w-1/3">
         <div class="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
           <g-image
-            src="~/assets/images/pokedex_thumb.png"
-            class="cursor-pointer"
-            @click="openPokedexProject"
+            src="~/assets/images/movix-project.png"
+            class="cursor-pointer max-h-64"
+            @click="openProject(projects[0].url)"
           />
           <div class="p-6">
             <h1
               class="title-font text-lg font-medium text-gray-900 mb-3 cursor-pointer"
-              @click="openPokedexProject"
+              @click="openProject(projects[0].url)"
             >
               {{ projects[0].title }}
             </h1>
             <p
               class="leading-relaxed mb-3"
               v-html="projects[0].description"
+            ></p>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/2 lg:w-1/3">
+        <div class="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
+          <g-image
+            src="~/assets/images/techcred-project.png"
+            class="cursor-pointer max-h-64"
+            @click="openProject(projects[1].url)"
+          />
+          <div class="p-6">
+            <h1
+              class="title-font text-lg font-medium text-gray-900 mb-3 cursor-pointer"
+              @click="openProject(projects[1].url)"
+            >
+              {{ projects[1].title }}
+            </h1>
+            <p
+              class="leading-relaxed mb-3"
+              v-html="projects[1].description"
+            ></p>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/2 lg:w-1/3">
+        <div class="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
+          <g-image
+            src="~/assets/images/pokedex_thumb.png"
+            class="cursor-pointer max-h-64"
+            @click="openProject(projects[2].url)"
+          />
+          <div class="p-6">
+            <h1
+              class="title-font text-lg font-medium text-gray-900 mb-3 cursor-pointer"
+              @click="openProject(projects[2].url)"
+            >
+              {{ projects[2].title }}
+            </h1>
+            <p
+              class="leading-relaxed mb-3"
+              v-html="projects[2].description"
             ></p>
           </div>
         </div>
@@ -39,6 +81,21 @@ export default {
       projects: [
         {
           thumbnail: "~/assets/images/pokedex_thumb.png",
+          title: "React Movie Catalog",
+          description:
+            "This project was developed in order to showcase an internal company tool that uses a graph DB. I was the lead Front End developer for the client side.",
+          url: "https://movix.lab.base22.com/",
+        },
+        {
+          thumbnail: "~/assets/images/techcred-project.png",
+          title: "Vue List Filter",
+          description:
+            "I made this for a State of Ohio <a href='https://techcred.ohio.gov/wps/portal/gov/techcred/home' target='_blank' class='text-purple-500'>agency</a>. It consists of list in which you can search/filter results by name and/or category.",
+          url:
+            "https://techcred.ohio.gov/wps/portal/gov/techcred/about/credential-list",
+        },
+        {
+          thumbnail: "~/assets/images/pokedex_thumb.png",
           title: "StencilJS Pokedex",
           description:
             "This is based from the <a href='https://codepen.io/siliconunicorn/pen/VqoxXP' class='text-purple-500' target='_blank'>React Pokedex Project</a>. This was just a learning project to dive into StencilJS. ",
@@ -48,8 +105,8 @@ export default {
     };
   },
   methods: {
-    openPokedexProject: function() {
-      window.open(this.projects[0].url, "_blank");
+    openProject: function(url) {
+      window.open(url, "_blank");
     },
   },
 };
